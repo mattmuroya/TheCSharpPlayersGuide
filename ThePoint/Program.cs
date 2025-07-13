39ee2ee356d26b322826414c16a1edb37b7dd95d
Matt Muroya <13994435+mattmuroya@users.noreply.github.com>
2025-07-13 10:35:54 -1000

    The Point

---
+++ ThePoint/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ThePoint;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = new Point(2, 3);
+        var b = new Point(-4, 0);
+        var c = new Point();
+        
+        a.Print();
+        b.Print();
+        c.Print();
+    }
+}
+
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point()
+    {
+        X = Y = 0;
+    }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"({X}, {Y})");
+    }
+}
